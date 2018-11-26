c4d2bbe69849b3d7552e0d472cb82b780f351181
aryanmshah <League@administorsimac.attlocal.net>
2018-11-25 17:28:56 -0800

    Started intro to arrays

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -3,15 +3,23 @@ package arrays;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
+		String[] array1 = {"abcd", "efgh", "ijkl", "mnopq", "rstu"};
 		
 		//2. print the third element in the array
+		System.out.println(array1[2]);
 		
 		//3. set the third element to a different value
+		array1[3] = "NOTijkl";
 		
 		//4. print the third element again
+		System.out.println(array1[2]);
 		
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
+		for(int i = 0; i < array1.length; i++)
+		{
+			System.out.print(array1[i] + " ");
+		}
 		
 		
 		//6. make an array of 50 integers
