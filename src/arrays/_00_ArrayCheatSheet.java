61ca831b09e5820ecf30d97b0c022b8506b8e18e
League <League@administorsimac.attlocal.net>
2018-12-09 17:34:46 -0800

    Finished array cheat sheet

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
@@ -18,18 +20,46 @@ public class _00_ArrayCheatSheet {
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
 		for(int i = 0; i < array1.length; i++)
 		{
-			System.out.print(array1[i] + " ");
+			System.out.println(array1[i] + " ");
 		}
 		
-		
 		//6. make an array of 50 integers
+		int[] ints = new int[50];
 		
 		//7. use a for loop to make every value of the integer array a random number
+		Random r = new Random();
+		for(int i = 0; i < ints.length; i++)
+		{
+			int j = r.nextInt(10);
+			ints[i] = j;
+		}
 		
 		//8. without printing the entire array, print only the smallest number in the array
+		int low = 9;
+		for(int i = 0; i < ints.length; i++)
+		{
+			if(ints[i] < low)
+			{
+				low = ints[i];
+			}
+		}
+		System.out.print(low + " ");
 		
 		//9 print the entire array to see if step 8 was correct
+		for(int i = 0; i < ints.length; i++)
+		{
+			System.out.print(ints[i]);
+		}
 		
 		//10. print the largest number in the array.
+		int high = 0;
+		for(int i = 0; i < ints.length; i++)
+		{
+			if(ints[i] > high)
+			{
+				high = ints[i];
+			}
+		}
+		System.out.print(" " + high);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,9 +1,15 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
+	public static void main(String[] args)
+	{
+		
 
 	//2. create an array of 5 robots.
+	Robot r = new Robot();
 
 	//3. use a for loop to initialize the robots.
 
@@ -19,4 +25,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+	}
 }
